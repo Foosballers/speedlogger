255acc0c54b674ba2e628995463e66c8d2e54254
Keith O <obrienkeithd@gmail.com>
2016-02-04 21:31:19 -0500

    beginnings of the api

---
+++ server/logroutes.js
@@ -0,0 +1,36 @@
+var express = require('express'),
+    logroutes = express.Router(),
+    bodyParser = require('body-parser');
+
+var template = {
+    upspeed: "double",
+    expectedupspeed: "double",
+    downspeed: "double",
+    expecteddownspeed: "double",
+    ping: "int",
+    provider: ["Verizon", "Comcast", "ATT", "Verizon Wireless", "T-Mobile", "ATT Wireless"],
+    location: "string"
+};
+
+logroutes.get('/', (req, res) => {
+    res.json({
+        items: [],
+        template: template
+    });
+});
+
+logroutes.get('/:id', (req, res) => {
+    res.json({
+        id: req.params.id
+    });
+});
+
+logroutes.post('/', (req, res) => {
+    var log = req.body;
+    log.links = {
+        self: "/api/logs/1"
+    };
+    res.status(201).json(log);
+});
+
+module.exports = logroutes;
